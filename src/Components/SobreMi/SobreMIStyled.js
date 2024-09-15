af78daf3e8240e5b0cc25d40e35fa8139cd1f2a1
jorgemc94 <jmaciasc1994@gmail.com>
2024-09-15 12:00:49 +0200

    add sobremi

---
+++ src/Components/SobreMi/SobreMIStyled.js
@@ -0,0 +1,43 @@
+import styled from "styled-components";
+
+
+export const SectionSobreMi = styled.section`
+    display: grid;
+    grid-template-columns: 40% 40%;
+    justify-content: space-between;
+    column-gap: 2em;
+    width: 80%;
+    margin: 0 auto;
+    
+`
+
+export const Title = styled.h1`
+    transform:translateX(10%);
+    margin-top: 3em;
+    padding-bottom: 1.5em;
+    color: rgb(22, 242, 179);
+    font-size: 2em;
+`;
+
+export const Text = styled.p`
+    line-height: 1.3;
+    font-size: 1.2em;
+`
+
+export const Imagen = styled.img`
+    border-radius: 2em;
+    width: 50%;
+    filter: grayscale(100%);
+    transition: all 0.3s ease;
+
+    &:hover {
+        transform: scale(1.1); 
+        filter: grayscale(0%);
+    }
+`;
+
+
+export const ContentImage = styled.div`
+    display:flex;
+    justify-content: center;
+`
--- src/Components/SobreMi/SobreMiComponent.jsx
+++ src/Components/SobreMi/SobreMiComponent.jsx
@@ -1,3 +1,4 @@
+import { SectionSobreMi, Title, Text, ContentImage, Imagen } from "./SobreMIStyled"
 
 
 export const SobreMiComponent = () => {
@@ -5,7 +6,15 @@ export const SobreMiComponent = () => {
 
     return(
         <>
-        
+            <Title>Sobre mí</Title>
+            <SectionSobreMi>
+                <div>
+                    <Text>Soy un Desarrollador Web Fullstack Junior y actualmente estoy desarrollando proyectos web trabajando a diario con programadores senior. Me gusta desarrollar código tanto en front end como en back end. El stack tecnológico que utilizo es JS con React y PHP con Laravel. También trabajo con Typescript y Testing (Unitario y Cypress). Puedo maquetar sitios web totalmente responsive (FlexBox, SaaS, …) y he desarrollado proyectos con bases de datos relacionales (SQL) y no relacionales (Mongo). Para el despliegue de mis proyectos utilizo AWS. Tengo experiencia desarrollando proyectos para turismo, ocio…</Text>
+                </div>
+                <ContentImage>
+                    <Imagen src="./Jorge.png" alt="Jorge"/>
+                </ContentImage>
+            </SectionSobreMi>
         </>
     )
 }
