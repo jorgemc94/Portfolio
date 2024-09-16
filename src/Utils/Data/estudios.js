d1c86a58e4e591c9cc421f4772adbf2fa9629eae
jorgemc94 <jmaciasc1994@gmail.com>
2024-09-16 20:58:17 +0200

    fix error estudiosData

--- src/Utils/Data/estudios.js
+++ src/Utils/Data/estudios.js
@@ -8,17 +8,17 @@ export const estudiosData = {
         {
             titulo:'Desarrollador Web Full Stack',
             fecha:'Arroyo de la Luz | Mayo 2022 - Agosto 2022',
-            descripcion:'creación aplicaciones web, tanto en el front-endcomo en el back-end Se utilizan lenguajes como HTML, CSS, JavaScript, y tecnologías como Node.js, bases de datos y APIs',
+            descripcion:'Creación aplicaciones web, tanto en el front-endcomo en el back-end Se utilizan lenguajes como HTML, CSS, JavaScript, y tecnologías como Node.js, bases de datos y APIs',
         },
         {
             titulo:'Administración Sistemas Informáticos en Red',
             fecha:'IES ÁGORA | Septiembre 2016 - Marzo 2018',
-            descripcion:' instalar, configurar y gestionar redes y servidores, asegurando su seguridad y correcto funcionamiento. También incluye el manejo de sistemas operativos, virtualización y servicios en la nube.',
+            descripcion:'Instalar, configurar y gestionar redes y servidores, asegurando su seguridad y correcto funcionamiento. También incluye el manejo de sistemas operativos, virtualización y servicios en la nube.',
         },
         {
             titulo:'Sistemas Microinformáticos y Redes',
             fecha:'IES SAN PEDRO DE ALCÁNTARA | Septiembre 2013 - Junio 2015',
-            descripcion:'instalar, configurar y mantener equipos informáticos y redes locales. Se cubren aspectos de hardware, sistemas operativos, redes, seguridad y resolución de problemas .',
+            descripcion:'Instalar, configurar y mantener equipos informáticos y redes locales. Se cubren aspectos de hardware, sistemas operativos, redes, seguridad y resolución de problemas .',
         },
     ]
 }
